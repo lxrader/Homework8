a753c7108bc145a7d63038bf5d5f5f07de22a654
lxrader <lrader2@utk.edu>
2019-10-28 09:58:53 -0400

    Creating a reactive slider input that changes when a go button is clicked with min and max cylinder values

--- Rader_Homework8/app.R
+++ Rader_Homework8/app.R
@@ -1,28 +1,37 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+############################
+# Homework 8
+#Due 11/1/2019
+#Creating a Shiny Web Application with mtcars dataset
+#Must have a range slider
+#Allows user to select x and y variables
+#Doesn't create plot until GO button is toggled
+############################
 
+
+#Loading in packages
 library(shiny)
+library(tidyverse)
+
+#Assigning min and max variables from cyl
+min.cyl <- min(mtcars$cyl)
+max.cyl <- max(mtcars$cyl)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Mtcars Data Set Viewer"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("cyl.adjuster",
+                        "Number of Cylinders",
+                        min.cyl,
+                        max.cyl,
+                        value = c(min.cyl, max.cyl)),
+            actionButton ("goButton",
+                          "Go")
         ),
 
         # Show a plot of the generated distribution
